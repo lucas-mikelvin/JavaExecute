1270153377a7e207ec7af55d45748d5800a4d993
lucas-mikelvin <61799626+lucas-mikelvin@users.noreply.github.com>
2020-11-28 17:52:47 -0300

    Ajustes no script de automação

--- projeto-ac3/scriptAutomacao.sh
+++ projeto-ac3/scriptAutomacao.sh
@@ -2,12 +2,12 @@
 
 echo "Verificando se possue java"
 
-which java | grep -q /urs/bin/java
+java -version
 
 if [[ $? = 0 ]]
 	then
-	echo "OK"
-	echo "Executando arquivo java"
+	echo "Java encontrado"
+	echo "Executando arquivo do projeto"
 	java -jar projeto-ac3-1.0-SNAPSHOT-jar-with-dependencies.jar
 else	
 	echo "Java não encontrado"
@@ -16,7 +16,7 @@ else
 	curl -s "https://get.sdkman.io" | bash
 	source "$HOME/.sdkman/bin/sdkman-init.sh"
 	sdk install java 8.0.272.j9-adpt
-	echo "Executando o java"
+	echo "Executando arquivo do projeto"
 	java -jar projeto-ac3-1.0-SNAPSHOT-jar-with-dependencies.jar	 	
 fi
 
